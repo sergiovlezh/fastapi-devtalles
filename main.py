eec9601cfec0aae0a920e2abfc3703e54a9404fa
Sergio Velez <sergio.vlezh@gmail.com>
2026-09-24 23:09:07 -0500

    feat: add seconds endpoint
    - define a list of hardcoded blogposts
    - add a /posts endpoint to return the list of blogposts

--- main.py
+++ main.py
@@ -5,6 +5,33 @@ app = FastAPI(
 )
 
 
+BLOG_POSTS = [
+    {
+        "id": 1,
+        "title": "Primer Post",
+        "content": "Este es el contenido del primer post.",
+        "author": "Juan Pérez",
+    },
+    {
+        "id": 2,
+        "title": "Segundo Post",
+        "content": "Este es el contenido del segundo post.",
+        "author": "María López",
+    },
+    {
+        "id": 3,
+        "title": "Tercer Post",
+        "content": "Este es el contenido del tercer post.",
+        "author": "Carlos García",
+    },
+]
+
+
 @app.get("/")
 def read_root():
     return {"message": "Bienevenido a Mini Blog!"}
+
+
+@app.get("/posts")
+def list_posts():
+    return {"data": BLOG_POSTS}
